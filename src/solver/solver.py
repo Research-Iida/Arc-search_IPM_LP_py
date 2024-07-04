bcd83ba20874a07072ab19a102e9280e108e2e9f
kuru-to <ideruf96@gmail.com>
2024-07-04 09:20:43 +0900

    refactor: 初期点を決めることに責務を持つクラス実装
    solver がまとめていたので分けた

--- src/solver/solver.py
+++ src/solver/solver.py
@@ -10,6 +10,7 @@ import numpy as np
 
 from ..logger import get_main_logger, indent
 from ..problem import LinearProgrammingProblemStandard as LPS
+from .initial_point_maker import IInitialPointMaker, YangInitialPointMaker
 from .optimization_parameters import OptimizationParameters
 from .solved_checker import AbsoluteSolvedChecker, RelativeSolvedChecker, SolvedChecker
 from .solved_data import SolvedDetail, SolvedSummary
@@ -22,10 +23,16 @@ class LPSolver(metaclass=ABCMeta):
     """LPを解くためのソルバーに関する抽象クラス"""
 
     solved_checker: SolvedChecker
+    # TODO: solver_config_section に改名
+    config_section: str
+    parameters: OptimizationParameters
+    initial_point_maker: IInitialPointMaker
 
     def _set_config_and_parameters(self, config_section: str):
         self.config_section = config_section
         self.parameters = OptimizationParameters.import_(config_section)
+        # TODO: 初期点決定方法は builder で初期化するときに決める
+        self.initial_point_maker = YangInitialPointMaker()
 
     @property
     def is_stopping_criteria_relative(self) -> bool:
@@ -45,6 +52,7 @@ class LPSolver(metaclass=ABCMeta):
         """
         self._set_config_and_parameters(config_section)
 
+        # TODO: ややこしい設定は builder クラスへ委譲
         if solved_checker is None:
             threshold = self.parameters.STOP_CRITERIA_PARAMETER
             if self.is_stopping_criteria_relative:
@@ -82,7 +90,7 @@ class LPSolver(metaclass=ABCMeta):
 
         # 初期点が設定されていなければ問題から設定
         if v_0 is None:
-            v_0 = self.initial_variables(problem)
+            v_0 = self.initial_point_maker.make_initial_point(problem)
 
         # 初期点の設定
         logger.info("Logging initial situation.")
@@ -112,95 +120,6 @@ class LPSolver(metaclass=ABCMeta):
             logger.warning(f"{solver_name} cannot solve this problem.")
 
         return aSolvedDetail
-
-    def initial_variables(self, problem: LPS) -> LPVariables:
-        """初期点の作成
-
-        x,s はともに非負の値になる必要がある
-        複数の決定方法によって初期点を作成し, 制約違反とmuのうちの最大値が最小だった初期点を初期点とする
-        決定方法について詳細は p122 7.3.1 参照
-
-        Returns:
-            LPVariables: 初期点 x, λ, s
-        """
-        v_m = self._initial_variables_by_Mehrotra(problem)
-        v_l = self._initial_variables_by_Lustig(problem)
-
-        def max_residual_mu(v_0: LPVariables, problem: LPS) -> float:
-            """制約残渣, mu のうち最大の値を出力"""
-            r_b = problem.residual_main_constraint(v_0.x)
-            r_c = problem.residual_dual_constraint(v_0.y, v_0.s)
-            return max(np.linalg.norm(r_b), np.linalg.norm(r_c), v_0.mu)
-
-        max_residual_mu_m = max_residual_mu(v_m, problem)
-        max_residual_mu_l = max_residual_mu(v_l, problem)
-        if max_residual_mu_m < max_residual_mu_l:
-            return v_m
-        else:
-            return v_l
-
-    def _initial_variables_by_Mehrotra(self, problem: LPS) -> LPVariables:
-        """Mehrotra の `On the implementation of a primal-dual interior point method` を
-        参考にした初期点作成
-        """
-        A = problem.A
-        c = problem.c
-        AA_T_inv = problem.AA_T_inv
-
-        yhat = AA_T_inv @ A @ c
-        s_hat = c - A.T @ yhat
-        x_hat = A.T @ AA_T_inv @ problem.b
-
-        delta_x = max([-1.1 * min(x_hat), 0])
-        delta_s = max([-1.1 * min(s_hat), 0])
-        x_delta = x_hat + delta_x
-        s_delta = s_hat + delta_s
-
-        delta_hat_x = delta_x
-        if sum(s_delta) != 0:
-            delta_hat_x += 0.5 * (x_delta).T.dot(s_delta) / sum(s_delta)
-        delta_hat_s = delta_s
-        if sum(x_delta) != 0:
-            delta_hat_s += 0.5 * (x_delta).T.dot(s_delta) / sum(x_delta)
-
-        output = LPVariables(x_hat + delta_hat_x, yhat, s_hat + delta_hat_s)
-        return output
-
-    def _initial_variables_by_Lustig(self, problem: LPS) -> LPVariables:
-        """Lustig etc. の
-        `On implementing Mehrotra's predictor corrector interior-point method for linear programming`
-        を参考にした初期点作成
-        """
-        A = problem.A
-        b = problem.b
-        c = problem.c
-
-        x_hat = A.T @ problem.AA_T_inv @ b
-        xi_1 = max(-100 * x_hat.min(), 100, np.linalg.norm(b, ord=1) / 100)
-        xi_2 = 1 + np.linalg.norm(c, ord=1)
-        x_0 = x_hat.copy()
-        s_0 = np.tile(xi_2, problem.n)
-        for i in range(problem.n):
-            x_0[i] = max(x_0[i], xi_1)
-            if c[i] > xi_2 or (c[i] >= 0 and c[i] < xi_2):
-                s_0[i] = c[i] + xi_2
-            elif c[i] < -xi_2:
-                s_0[i] = -c[i]
-        return LPVariables(x_0, np.zeros(problem.m), s_0)
-
-    def _initial_variables_constant(self, problem: LPS) -> LPVariables:
-        """定数で与えられた初期点の作成
-
-        (x,y,s) = (C,0,C) となる
-
-        Returns:
-            LPVariables: 初期点 x, λ, s
-        """
-        return LPVariables(
-            np.ones(problem.n) * self.initial_point_scale,
-            np.zeros(problem.m),
-            np.ones(problem.n) * self.initial_point_scale,
-        )
 
     def log_initial_problem_information(self, problem_0: LPS):
         """最初の段階での問題に関するロギングの実行"""
