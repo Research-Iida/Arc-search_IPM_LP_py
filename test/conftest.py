0411bbd4f735c9e62c3a7f479309cf06dc9ec6e4
kuru-to <ideruf96@gmail.com>
2024-07-06 16:57:15 +0900

    HHL が死んでしまわれたので test もしない

--- test/conftest.py
+++ test/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from src.infra.julia.setup_julia import setup_julia
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def fixture_setup_julia():
     """julia の activate は1セッションで1回実行すれば十分"""
     setup_julia()
