719a9fe3ee20320132d223f840cce8ef95f4ced5
kuru-to <ideruf96@gmail.com>
2024-07-06 15:58:04 +0900

    test commit

--- test/conftest.py
+++ test/conftest.py
@@ -3,7 +3,7 @@ import pytest
 from src.infra.julia.setup_julia import setup_julia
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def fixture_setup_julia():
     """julia の activate は1セッションで1回実行すれば十分"""
     setup_julia()
