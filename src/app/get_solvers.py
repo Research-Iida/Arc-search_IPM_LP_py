23b85be7c45bb9279618b5bbac6abac9ddb16d97
kuru-to <ideruf96@gmail.com>
2025-12-11 14:38:23 +0900

    feat: yang の初期点での実行を追加

--- src/app/get_solvers.py
+++ src/app/get_solvers.py
@@ -81,7 +81,7 @@ target_config_sections_by_algorithm: dict[str, list[str]] = {
         # "INEXACT_ARC_HHLJULIA_NES_IN_SERVER",
     ],
     "inexact_arc_without_proof": ["INEXACT_ARC_CG_NES_CONSTANT"],
-    "CPLEX": [default_section],
+    "CPLEX": ["SOLVE_LARGE_PROBLEMS"],
 }
 
 
